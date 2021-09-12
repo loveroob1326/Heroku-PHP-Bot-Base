c4877efa2b4cfc808abef7b44edd078cf901308d
PlusClay <60856796+PlusClay@users.noreply.github.com>
2021-09-12 00:51:50 -0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -7,7 +7,7 @@ $chat_id = $message->chat->id;
 $text = $message->text;
 
 define('TOKEN' , "1975042900:AAFJQPby8twN61oYhvASyaZW6ejYxmm2boU")
-$admin = '799041666';
+$admin = 799041666;
 
 function bot($method, $data){
     $api = "https://api.telegram.org/bot". TOKEN ."/$method";
@@ -20,18 +20,18 @@ function bot($method, $data){
 }
 
 if($text = "/start" || "/Start"){
-    bot('SendMessage',[
+    bot('sendmessage',[
         'chat_id' => $chat_id,
         'text' => "Hi Send Your Message"
     ]);
 }
 else{
-    bot("ForwardMessage",[
+    bot("forwardmessage",[
         "from_chat_id" => $chat_id,
         "message_id" => $message_id,
         "chat_id" => $admin
     ]);
-    bot("SendMessage",[
+    bot("sendmessage",[
         "chat_id" => $chat_id,
         "text" => "Your Message Has Been Send !"
     ]);
