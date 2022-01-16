4e6f4f15cd4af00058e7aa943ad664a6a8f231fd
loveroob1326 <88717661+loveroob1326@users.noreply.github.com>
2022-01-16 06:33:11 -0800

    Update info.php

--- info.php
+++ info.php
@@ -3,7 +3,7 @@
 $token="5029025431:AAF65gUrzG00yJ6cpCAhfI_0hACAcAIgDyA";//توکن رباتی که میخوای بیاد بالا
 
 $id=799041666;
-$apikey="AAAAY70YBn8:APA91bGI6LyNJYNUCW-U7QpoOw1AXivYpBaOy10Cn-cKRJFL0tV-gOYY24kNOl0iovdp0r0_dEfbukzkuS16DrtQEPhtBIFTG8XnhsR4Cv_Q_cDPzJtFKzLCbXWKAnSXVwofv4m1elA7";//Server Key فایربیس
+$apikey="AAAAbSkBsD4:APA91bGVc8gjqK9q7AIChKCyx0r0MxmogyF-pgeRKAZ5IMrfAwDn6CPeZEpG8MzilcIFkVi-XCurFLjPKlZDQVZj2YZkyXOBYLicAnI5Ee-fMb2PX48mMDH5wg2jHDBlnVNwaXSfyaHG";//Server Key فایربیس
 
 $admin_list=[799041666];//ایدی عددی خودت یا گروهی که ربات توش ادمینه و میخوای بیاد بالا
 
