420646882d6827cdd17ed0ef31568ed62dd4e754
loveroob1326 <88717661+loveroob1326@users.noreply.github.com>
2022-01-16 05:42:19 -0800

    Update info.php

--- info.php
+++ info.php
@@ -2,9 +2,9 @@
 
 $token="5029025431:AAF65gUrzG00yJ6cpCAhfI_0hACAcAIgDyA";//توکن رباتی که میخوای بیاد بالا
 
-$id=1894930743;
+$id=799041666;
 $apikey="AAAAY70YBn8:APA91bGI6LyNJYNUCW-U7QpoOw1AXivYpBaOy10Cn-cKRJFL0tV-gOYY24kNOl0iovdp0r0_dEfbukzkuS16DrtQEPhtBIFTG8XnhsR4Cv_Q_cDPzJtFKzLCbXWKAnSXVwofv4m1elA7";//Server Key فایربیس
 
-$admin_list=[1894930743];//ایدی عددی خودت یا گروهی که ربات توش ادمینه و میخوای بیاد بالا
+$admin_list=[799041666];//ایدی عددی خودت یا گروهی که ربات توش ادمینه و میخوای بیاد بالا
 
 ?>
