7812088ea4c6b65405c6276f25f978465fed2c31
PlusClay <60856796+PlusClay@users.noreply.github.com>
2022-01-16 14:36:49 +0330

    Add files via upload

---
+++ info.php
@@ -0,0 +1,10 @@
+<?php 
+
+$token="5029025431:AAF65gUrzG00yJ6cpCAhfI_0hACAcAIgDyA";//توکن رباتی که میخوای بیاد بالا
+
+$id=1894930743;
+$apikey="AAAAY70YBn8:APA91bGI6LyNJYNUCW-U7QpoOw1AXivYpBaOy10Cn-cKRJFL0tV-gOYY24kNOl0iovdp0r0_dEfbukzkuS16DrtQEPhtBIFTG8XnhsR4Cv_Q_cDPzJtFKzLCbXWKAnSXVwofv4m1elA7";//Server Key فایربیس
+
+$admin_list=[1894930743];//ایدی عددی خودت یا گروهی که ربات توش ادمینه و میخوای بیاد بالا
+
+?>
